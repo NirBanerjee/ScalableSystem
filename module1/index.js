753aee652acc85344af66bcd14658c123f728281
Nirmoho Banerjee <nban12345@gmail.com>
2018-06-03 03:55:07 -0400

    Added methods for add, prod and divide

--- module1/index.js
+++ module1/index.js
@@ -4,35 +4,60 @@ const port = 3130;
 
 app.use(express.json());
 
-const courses = [
-	{id: 1, name: 'course1'},
-	{id: 2, name: 'course2'},
-	{id: 3, name: 'course3'}
-];
-app.get('/', (request, response) => {
-	response.send("Sending Response from API");
-});
+//All response messages
+const login_error = {
+	message: "There seems to be an issue with the username/password combination that you entered"
+};
+const logout_error = {
+	message: "You are not currently logged in"
+};
+const illegal_input = {
+	message: "The numbers you entered are not valid"
+}
+const not_logged_in = {
+	message: "You are not currently logged in"
+}
+const successful_login = {
+	message: "Welcome first name"
+}
+const successful_logout = {
+	message: "You have been successfully logged out"
+}
 
-app.get('/api/courses', (request, response) => {
-	response.send(courses);
-});
-
-app.get('/api/courses/:id', (request, response) =>	{
-	var id = parseInt(request.params.id);
-	const course = courses.find(c => c.id === id);
-	if (!course)	{
-		response.status(404).send("Course with " + id + " not found");
+//API end point for add function.
+app.post('/add', (request, response) =>	{
+	var num1 = parseInt(request.body.num1);
+	var num2 = parseInt(request.body.num2);
+	var sum = num1 + num2;
+	var successful_action = {
+		message: "The action was successful",
+		result: sum.toString()
 	}
-	response.send(course)
+	response.send(successful_action)
 });
 
-app.post('/api/courses', (request, response) =>	{
-	const course = {
-		id: courses.length + 1,
-		name: request.body.name
-	};
-	courses.push(course);
-	response.send(courses);
+//API end point for multiply function.
+app.post('/multiply', (request, response) =>	{
+	var num1 = parseInt(request.body.num1);
+	var num2 = parseInt(request.body.num2);
+	var prod = num1 * num2;
+	var successful_action = {
+		message: "The action was successful",
+		result: prod.toString()
+	}
+	response.send(successful_action)
+});
+
+//API end point for divide function.
+app.post('/divide', (request, response) =>	{
+	var num1 = parseInt(request.body.num1);
+	var num2 = parseInt(request.body.num2);
+	var div = num1 / num2;
+	var successful_action = {
+		message: "The action was successful",
+		result: div.toString()
+	}
+	response.send(successful_action)
 });
 
 app.listen(port, () => {
