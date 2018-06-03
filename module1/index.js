eef9e6aa1f4603fec215ee3f3588c9ddb8f137f7
Nirmoho Banerjee <nban12345@gmail.com>
2018-06-03 19:10:25 -0400

    Added Login functionality

--- module1/index.js
+++ module1/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const Joi = require('joi');
+const session = require('express-session');
 const app = express();
 const port = 3130;
 
 app.use(express.json());
+app.use(session({
+	secret: "jncksdc9013eu0", 
+	resave: "false",
+	saveUninitialized: "true"
+}));
+
+//Users
+const users = [
+	{
+		firstName: "Henry",
+		lastName: "Smith",
+		userName: "hsmith",
+		password: "smith"
+	},
+	{
+		firstName: "Tim",
+		lastName: "Bucktoo",
+		userName: "tbucktoo",
+		password: "bucktoo"
+	}
+];
 
 //All response messages
 const login_error = {
@@ -18,9 +40,7 @@ const illegal_input = {
 const not_logged_in = {
 	message: "You are not currently logged in"
 };
-const successful_login = {
-	message: "Welcome first name"
-};
+
 const successful_logout = {
 	message: "You have been successfully logged out"
 };
@@ -30,62 +50,100 @@ const input_schema = {
 	num2: Joi.number().required()
 }
 
-const input_schema_div = {
-	num1: Joi.number().required(),
-	num2: Joi.number().min(1).required()
+const login_schema = {
+	username: Joi.string().required(),
+	password: Joi.string().required()
 }
-//API end point for add function.
-app.post('/add', (request, response) =>	{
-	const validation_result = Joi.validate(request.body, input_schema);
+
+//API end point for login function
+app.post('/login', (request, response) => {
+	const validation_result = Joi.validate(request.body, login_schema);
 
 	if (validation_result.error)	{
-		response.send(illegal_input)
-	}	else	{
-		var num1 = parseFloat(request.body.num1);
-		var num2 = parseFloat(request.body.num2);
-		var sum = num1 + num2;
-		var successful_action = {
-			message: "The action was successful",
-			result: sum.toString()
-		}
-		response.send(successful_action)
+		return response.status(200).send(login_error)
 	}
+
+	var username = request.body.username;
+	var password = request.body.password;
+
+	const user = users.find(u => (u.userName === username && u.password === password));
+	if (!user)	{
+		return response.status(200).send(login_error);
+	}
+
+	request.session.user = user;
+	const successful_login = {
+		message: "Welcome " + user.firstName
+	};
+	response.send(successful_login)
+});
+
+//API end point for logout function
+app.post('/logout', (request, response) => {
+
+});
+
+//API end point for add function.
+app.post('/add', (request, response) =>	{
+	if (!request.session.user)	{
+		return response.status(200).send(not_logged_in);
+	}
+
+	const validation_result = Joi.validate(request.body, input_schema);
+	if (validation_result.error)	{
+		return response.status(200).send(illegal_input)
+	}	
+	var num1 = parseFloat(request.body.num1);
+	var num2 = parseFloat(request.body.num2);
+	var sum = num1 + num2;
+	var successful_action = {
+		message: "The action was successful",
+		result: sum.toString()
+	}
+	response.send(successful_action)
 });
 
 //API end point for multiply function.
 app.post('/multiply', (request, response) =>	{
-	const validation_result = Joi.validate(request.body, input_schema);
-
-	if (validation_result.error)	{
-		response.send(illegal_input)
-	}	else	{
-		var num1 = parseFloat(request.body.num1);
-		var num2 = parseFloat(request.body.num2);
-		var prod = num1 * num2;
-		var successful_action = {
-			message: "The action was successful",
-			result: prod.toString()
-		}
-		response.send(successful_action)
+	if (!request.session.user)	{
+		return response.status(200).send(not_logged_in);
 	}
+
+	const validation_result = Joi.validate(request.body, input_schema);
+	if (validation_result.error)	{
+		return response.status(200).send(illegal_input)
+	}	
+	var num1 = parseFloat(request.body.num1);
+	var num2 = parseFloat(request.body.num2);
+	var prod = num1 * num2;
+	var successful_action = {
+		message: "The action was successful",
+		result: prod.toString()
+	}
+	response.send(successful_action)
 });
 
 //API end point for divide function.
 app.post('/divide', (request, response) =>	{
-	const validation_result = Joi.validate(request.body, input_schema_div);
-
-	if (validation_result.error)	{
-		response.send(illegal_input)
-	}	else	{
-		var num1 = parseFloat(request.body.num1);
-		var num2 = parseFloat(request.body.num2);
-		var quo = num1 / num2;
-		var successful_action = {
-			message: "The action was successful",
-			result: quo.toString()
-		}
-		response.send(successful_action)
+	if (!request.session.user)	{
+		return response.status(200).send(not_logged_in);
 	}
+
+	const validation_result = Joi.validate(request.body, input_schema);
+	if (validation_result.error)	{
+		return response.status(200).send(illegal_input)
+	}	
+	var num1 = parseFloat(request.body.num1);
+	var num2 = parseFloat(request.body.num2);
+	if (num2 == 0.0)	{
+		return response.status(200).send(illegal_input)
+	}
+	var quo = num1 / num2;
+	var successful_action = {
+		message: "The action was successful",
+		result: quo.toString()
+	}
+	response.send(successful_action)
 });
 
 app.listen(port, () => {
