67366a6f82ad36926e3dd337186294bddebc8b04
Nirmoho Banerjee <nban12345@gmail.com>
2018-06-03 01:19:23 -0400

    Added Git methods

--- module1/index.js
+++ module1/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const port = 3130;
 
-app.get()
-app.post()
-app.put()
-app.delete()
+app.get('/', (request, response) => {
+	response.send("Sending Response from API");
+});
+
+app.get('/api/courses', (request, response) => {
+	response.send([1, 2, 3]);
+});
+
+app.listen(port, () => {
+	console.log("Listening on port - " + port);
+});
